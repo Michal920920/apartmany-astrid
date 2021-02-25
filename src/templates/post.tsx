35ad47d7d3d623543b6a6bf88d7d1188e91f63b1
michal92 <michal.92@email.cz>
2021-02-25 22:43:56 +0100

    blog

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -8,7 +8,9 @@ const Post = (data) => {
 	if (!data || !data.data) {
 		return null;
 	}
-	const values = data.data.allPrismicBlog.edges[0].node.data;
+	const textEditor = data.data.allPrismicBlog.nodes[0].data.body[0].primary.text_editor;
+	const values = data.data.allPrismicBlog.nodes[0].data;
+	console.log(textEditor);
 	return (
 		<Layout>
 			<section className="breadcrumb-area" style={{backgroundImage: "url(" + values.main_image.url + ")"}}>
@@ -33,24 +35,9 @@ const Post = (data) => {
 										<li><i className="fal fa-user"/>{values.blog_author}</li>
 										<li><i className="fal fa-calendar-alt"/>{moment(values.blog_date).format('d. M. YYYY')}</li>
 									</ul>
-									{values.body.forEach((item) => {
-										if (item.slice_type == 'image') {
-											return (
-												''
-											)
-										}
-										if (item.slice_type == 'blogovy_editor') {
-											return (
-												''
-											)
-										}
-										if (item.slice_type == 'citace') {
-											return (
-												''
-											)
-										}
+									{textEditor.map((item, index) => {
+										return <RichText key={index} render={item}/>
 									})}
-									<RichText render={data.address}/>
 								</div>
 							</div>
 						</div>
@@ -64,37 +51,37 @@ export default Post
 export const pageQuery = graphql`
     query PostBySlug($slug: String!) {
         allPrismicBlog(filter: {slugs: {eq: $slug}}) {
-            edges {
-                node {
-                    data {
-                        body {
-                            items {
-                                blog_image {
-                                    url
+            nodes {
+                data {
+                    blog_date
+                    main_image {
+                        url
+                    }
+                    blog_author
+                    blog_anotation {
+                        text
+                    }
+                    blog_title {
+                        text
+                    }
+                    body {
+                        primary {
+                            text_editor {
+                                spans {
+                                    end
+                                    start
+                                    type
                                 }
-                                autor {
-                                    text
-                                }
-                                nadpis {
-                                    text
-                                }
-                                text {
-                                    text
+                                text
+                                type
+                                url
+                                dimensions {
+                                    height
+                                    width
                                 }
                             }
-                            slice_type
                         }
-                        blog_title {
-                            text
-                        }
-                        blog_date
-                        blog_author
-                        blog_anotation {
-                            text
-                        }
-                        main_image {
-                            url
-                        }
+                        slice_type
                     }
                 }
             }
